a81492100875ebe3815da081123a7cce9a438964
AsyaSkoraya <153989033+AsyaSkoraya@users.noreply.github.com>
2024-05-29 23:33:25 +0500

    tests

--- backend-resources/src/test/java/com/itm/space/backendresources/BaseIntegrationTest.java
+++ backend-resources/src/test/java/com/itm/space/backendresources/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.itm.space.backendresources.api.request.UserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
